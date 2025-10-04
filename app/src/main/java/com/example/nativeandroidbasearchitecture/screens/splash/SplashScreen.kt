55a1f5958ca729933da19929373671c5efaf5a56
shahidshaikh <shahid.shaikh@reelsaga.in>
2025-10-04 13:38:22 +0530

    add new color definitions and enhance SplashScreen with gradient background

--- app/src/main/java/com/example/nativeandroidbasearchitecture/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/example/nativeandroidbasearchitecture/screens/splash/SplashScreen.kt
@@ -6,19 +6,25 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.nativeandroidbasearchitecture.R
 import com.example.nativeandroidbasearchitecture.ui.theme.Color00954D
+import com.example.nativeandroidbasearchitecture.ui.theme.ColorFFFFFF
+import com.example.nativeandroidbasearchitecture.ui.theme.ColorFFFFFF_28
 import com.example.nativeandroidbasearchitecture.ui.theme.fontBoldMontserrat
 import kotlinx.coroutines.delay
 
@@ -38,6 +44,18 @@ fun SplashScreen(gotoLoginOptionScreen: () -> Unit) {
             Image(
                 painter = painterResource(id = R.drawable.rego_brandmark_1),
                 contentDescription = "Rego Logo",
+            )
+            val gradientColors = listOf(ColorFFFFFF_28(), ColorFFFFFF.copy(0.7f), ColorFFFFFF_28())
+            Box(
+                modifier = Modifier
+                    .alpha(0.3f)
+                    .width(170.dp)
+                    .height(1.dp)
+                    .background(
+                        brush = Brush.linearGradient(
+                            colors = gradientColors
+                        )
+                    )
             )
             Spacer(modifier = Modifier.size(21.dp))
             Text(
